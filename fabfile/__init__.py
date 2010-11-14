86c3e3376eb57c4b6ccf02b174366f3d4e97624b
Travis Swicegood <development@domain51.com>
2010-11-14 15:13:21 -0600

    move into the appropriate place

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -2,13 +2,6 @@ from ._utils import *
 
 @task
 def test():
-    try:
-        from d51.django.virtualenv.test_runner import run_tests
-    except ImportError:
-        import sys
-        sys.stderr.write("Please install d51.django.virtualenv.test_runner to run these tests\n")
-        sys.exit(-1)
-
     settings = {
         'INSTALLED_APPS': (
             'armstrong.esi',
--- fabfile/_utils.py
+++ fabfile/_utils.py
@@ -3,3 +3,11 @@ from fabric.decorators import task
 
 import os, sys
 sys.path[0:0] = [os.path.join(os.path.realpath('.'), '..'), ]
+
+try:
+    from d51.django.virtualenv.test_runner import run_tests
+except ImportError:
+    import sys
+    sys.stderr.write("This project requires d51.django.virtualenv.test_runner\n")
+    sys.exit(-1)
+
