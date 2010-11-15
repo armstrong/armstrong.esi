3683dcb5153d4faf929b2993ccef3724b52580b4
Travis Swicegood <development@domain51.com>
2010-11-15 12:33:16 -0600

    add ability to launch a shell to fabfile

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,13 +1,23 @@
 from ._utils import *
 
+settings = {
+    'INSTALLED_APPS': (
+        'armstrong.esi',
+        'armstrong.esi.tests.esi_support',
+    ),
+    'TEMPLATE_CONTEXT_PROCESSORS': [
+        'armstrong.esi.context_processors.esi',
+    ],
+    'ROOT_URLCONF': 'armstrong.esi.tests.esi_support.urls',
+}
+
 @task
 def test():
-    settings = {
-        'INSTALLED_APPS': (
-            'armstrong.esi',
-            'armstrong.esi.tests.esi_support',
-        ),
-        'ROOT_URLCONF': 'armstrong.esi.tests.esi_support.urls',
-    }
     run_tests(settings, 'esi_support', 'esi')
 
+@task
+def shell():
+    from d51.django.virtualenv.base import VirtualEnvironment
+    v = VirtualEnvironment()
+    v.run(settings)
+    v.call_command('shell')
