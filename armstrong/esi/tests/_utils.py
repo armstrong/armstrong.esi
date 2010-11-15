1e10fa30998f63359ddd26d9804bd32a837c2cab
Travis Swicegood <development@domain51.com>
2010-11-15 12:34:26 -0600

    Add in a decorator for generating fake request objects for test cases

--- armstrong/esi/tests/_utils.py
+++ armstrong/esi/tests/_utils.py
@@ -1,6 +1,19 @@
 from django.conf import settings
+from django.http import HttpRequest
 from django.test import TestCase as DjangoTestCase
 import fudge
+
+def with_fake_request(func):
+    def inner(self, *args, **kwargs):
+        request = fudge.Fake(HttpRequest)
+        fudge.clear_calls()
+
+        result = func(self, request, *args, **kwargs)
+
+        fudge.verify()
+        fudge.clear_expectations()
+        return result
+    return inner
 
 class TestCase(DjangoTestCase):
     def setUp(self):
