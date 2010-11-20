74c67c29c1c912e41cec7a27c6be47432e991549
Niran Babalola <niran@niran.org>
2010-11-19 19:21:58 -0600

    Add http_client tests

--- armstrong/esi/tests/esi_support/views.py
+++ armstrong/esi/tests/esi_support/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def hello(request, number=None):
-    return HttpResponse(number or '')
+    if request.GET:
+        pairs = ('%s = %s' % (key, request.GET[key]) for key in sorted(request.GET))
+        return HttpResponse(', '.join(pairs))
+    return HttpResponse(number or 'Hello World!')
