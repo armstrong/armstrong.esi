97961319d6fb5af6287f56bfce889ebafb817fd6
Travis Swicegood <development@domain51.com>
2010-12-03 18:38:34 -0600

    add in a base level setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='armstrong.esi',
+    version='0.1',
+    description='Base functionality that needs to be shared widely',
+    author='Texas Tribune',
+    author_email='tech@texastribune.org',
+    url='http://github.com/texastribune/armstrong.esi/',
+    packages=[
+        'armstrong',
+        'armstrong.esi',
+    ],
+
+    install_requires=[
+        'setuptools',
+    ],
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+)
